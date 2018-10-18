fde36bf4b9a30c96bfc87f08ab33b2cb2fcfc8eb
jkremes <only1truth333@gmail.com>
2018-10-18 14:35:28 -0400

    Added editable and showing :user for the article Serializer

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :description, :size, :color
+  attributes :id, :description, :size, :color, :editable, :user
+
+  def editable
+    scope == object.user
+  end
 end
